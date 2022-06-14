26062f20d47aa02f89bd33e1e0d50f8fcff82418
FabioSntos <fabio_santos_2001@outlook.com>
2022-06-14 20:08:02 -0300

    .

---
+++ src/main/java/br/ce/loca/services/LocacaoService.java
@@ -0,0 +1,34 @@
+package br.ce.loca.services;
+
+import static br.ce.loca.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import br.ce.loca.entities.Filme;
+import br.ce.loca.entities.Locacao;
+import br.ce.loca.entities.Usuario;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
