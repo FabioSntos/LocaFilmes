1c849738ccd46cae0c3ec622d0c65223713bdb12
FabioSntos <fabio_santos_2001@outlook.com>
2022-06-15 20:19:43 -0300

    refatoração dos testes de locacao

--- src/test/java/br/ce/loca/services/LocacaoServiceTest.java
+++ src/test/java/br/ce/loca/services/LocacaoServiceTest.java
@@ -4,16 +4,42 @@ import br.ce.loca.entities.Filme;
 import br.ce.loca.entities.Locacao;
 import br.ce.loca.entities.Usuario;
 
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static br.ce.loca.utils.DataUtils.*;
 
 public class LocacaoServiceTest {
 
     @Test
-    public void deveAlugarFilme() {
+    public void verifyLocationMoviesPrice() {
         LocacaoService locacaoService = new LocacaoService();
-        Locacao locacao = locacaoService.alugarFilme(new Usuario("Fabio"), new Filme("nome", 1, 10.00));
+        Filme filme = new Filme("filme 1", 2, 5.00);
+        Locacao locacao = locacaoService.alugarFilme(new Usuario("Fabio"), filme);
 
-        Assert.assertEquals("nome", locacao.getFilme().getNome());
+        assertThat(locacao.getValor(), is(equalTo(5.00)));
+    }
+
+    @Test
+    public void shouldReturnLocationDateEqualNow() {
+        LocacaoService locacaoService = new LocacaoService();
+        Filme filme = new Filme("filme 1", 2, 5.00);
+        Locacao locacao = locacaoService.alugarFilme(new Usuario("Fabio"), filme);
+
+        assertThat(isMesmaData(locacao.getDataLocacao(), new Date()),is(true));
+    }
+
+    @Test
+    public void shouldBeTrueThatReturnDateIsTomorrow() {
+        LocacaoService locacaoService = new LocacaoService();
+        Filme filme = new Filme("filme 1", 2, 5.00);
+        Locacao locacao = locacaoService.alugarFilme(new Usuario("Fabio"), filme);
+
+        assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)),is(true));
     }
 }
